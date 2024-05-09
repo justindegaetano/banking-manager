06fc5bd0918fecb82f9d4357af5f419c7fb6eacd
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2024-05-09 13:52:00 -0400

    Add Header greeting TotalBalanceBox to Home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 
 const Home = () => {
+  const loggedIn = { firstName: 'Justin' };
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'guest'}
+            subtext='Access and manage your accounts and transactions efficiently.'
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={300.69}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
