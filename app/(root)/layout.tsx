5f25ca8b09ed2bd6a8658448bc05a866e1c569b9
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2024-05-13 11:14:49 -0400

    Add left navigation sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,34 @@
+import MobileNav from "@/components/ui/MobileNav";
+import Sidebar from "@/components/ui/Sidebar";
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Justin', lastName: 'DeGaetano' };
+
   return (
-    <main>
-        Sidebar
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image
+              src='icons/logo.svg'
+              width={30}
+              height={30}
+              alt='menu icon'
+            />
+            <div>
+              <MobileNav user={loggedIn} />
+            </div>
+          </div>
         {children}
+        </div>
     </main>
   );
 }
