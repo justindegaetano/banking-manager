e83b532cedbb61a56a2b889659c199e2c8237e25
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2024-05-24 12:52:37 -0400

    Add Plaid & Dwolla Functionality

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -23,6 +23,7 @@ import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from '../PlaidLink';
 
 const AuthForm = ({ type }: {type: string }) => {
   const router = useRouter();
@@ -47,7 +48,21 @@ const AuthForm = ({ type }: {type: string }) => {
     try {
       // Sign up w Appwrite $ create Plaid link token
       if (type === 'sign-up') {
-        const newUser = await signUp(data);
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          zipCode: data.zipCode!,
+          dob: data.dob!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password,
+
+        }
+
+        const newUser = await signUp(userData);
 
         setUser(newUser);
       }
@@ -100,7 +115,7 @@ const AuthForm = ({ type }: {type: string }) => {
       </header>
       {user ? (
         <div className='flex flex-col gap-4'>
-          {/* PlaidLink */}
+          <PlaidLink user={user} variant='primary' />
         </div>
       ) : (
         <>
